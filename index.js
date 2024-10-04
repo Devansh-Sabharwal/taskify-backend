4408d872e77fba5f02655621bf0af5605cbd80dc
Devansh Sabharwal <sabharwaldevansh04@gmail.com>
2024-10-04 13:43:49 +0530

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const todoRoutes = require('./routes/todo');
 const userRoutes = require('./routes/user');
@@ -7,7 +8,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
-
+app.use(cors());  
 
 app.use(express.json());
 
